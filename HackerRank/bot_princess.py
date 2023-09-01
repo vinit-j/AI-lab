da1c07eb6b5fa49619b036f58eae6dcfe6fcd694
vinit-j <111049801+vinit-j@users.noreply.github.com>
2023-09-01 20:31:43 +0530

    Create bot_princess.py

---
+++ HackerRank/bot_princess.py
@@ -0,0 +1,41 @@
+def displayPathtoPrincess(n,grid):
+    pos_col = {}
+    pos_row = {}
+    not_find = True
+
+    for i in range(n):
+        line = len(grid[i])
+        for j in range(line):
+            if grid[i][j] == 'm':
+                pos_row['m'] = i
+                pos_col['m'] = j
+            elif grid[i][j] == 'p':
+                pos_row['p'] = i
+                pos_col['p'] = j
+
+    while not_find:
+        if pos_row['m'] < pos_row['p']:
+            pos_row['m'] = pos_row['m'] + 1
+            print('DOWN')
+        elif pos_row['m'] > pos_row['p']:
+            pos_row['m'] = pos_row['m'] - 1
+            print('UP')
+
+        if pos_col['m'] < pos_col['p']:
+            pos_col['m'] = pos_col['m'] + 1
+            print('RIGHT')
+        elif pos_col['m'] > pos_col['p']:
+            pos_col['m'] = pos_col['m'] - 1
+            print('LEFT')
+        
+        if pos_col['m'] == pos_col['p'] and pos_row['m'] == pos_row['p']:
+            not_find = False
+
+m = 3
+grid = [
+    "---",
+    "-m-",
+    "p--"
+]
+
+displayPathtoPrincess(m, grid)
